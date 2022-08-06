aa0ec9e534968122342d1738274cea5840d60013
Buntu Ngcebetsha <bngcebetsha@sarao.ac.za>
2022-08-06 09:56:46 +0200

    Put it all in one folder

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>I am registered for ICT2613 in 2022. ICT2613 has 3 assignments.</title>
-    </head>
-    <body>
-        <?php include 'menu.inc'; ?>
-    </body>
-</html>
