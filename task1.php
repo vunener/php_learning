6b15f59b1a0974071386265b71825eafa23c6cbe
Buntu Ngcebetsha <bngcebetsha@sarao.ac.za>
2022-08-06 09:49:25 +0200

    Initial files

---
+++ task1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Task1</title>
+    </head>
+    <body>
+        <?php include 'menu.inc'; ?>
+    </body>
+    <iframe src="task1.txt" height="400" width="1200">
+    Your browser does not support iframes. </iframe>
+</html>
