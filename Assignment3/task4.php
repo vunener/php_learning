1638478e801e3f24965cf05394f4d2acc97ffaaa
Buntu Ngcebetsha <bngcebetsha@sarao.ac.za>
2022-08-09 03:33:34 +0200

    easily render each task page
    in each page, include links to the other pages

--- Assignment3/task4.php
+++ Assignment3/task4.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <iframe src="task4.txt" height="400" width="1200">
-            Your browser does not support iframes.
-        </iframe>
-    </body>
-</html>
+<?php include 'menu.inc'; ?>
+<iframe src="task4.txt" height="400" width="1200">
+    Your browser does not support iframes.
+</iframe>
