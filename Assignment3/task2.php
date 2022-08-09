2abda3afa9c6a566f2dcb216f4dbddc487f8b99a
Buntu Ngcebetsha <bngcebetsha@sarao.ac.za>
2022-08-09 18:16:56 +0200

    task 3 and a little task2
    Will have to tackle tasks 4 and 5 soonest

--- Assignment3/task2.php
+++ Assignment3/task2.php
@@ -1,71 +1,64 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+<form method="get" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+Product1: <input type="checkbox" name="product1"><br>
+Product2: <input type="checkbox" name="product2"><br>
+<input type="submit" name="Submit"><br>
+</form>
+</body>
+</html>
+
 <?php
 include 'menu.inc';
-function selectProduct() {
-    if ($productName == 'product1') {
-        $_GET['product'] = 'product1';
-        $productSelected = 'product1';
-        $basePrice = 50;
-    } elseif ($productName == 'product2') {
-        $_GET['product'] = 'product2';
-        $productSelected = 'product2';
-        $basePrice = 510;
-    } else {
-        $productSelected = "";
-        $basePrice = 0;
-        echo "Please select a product to buy";
-    }
-    return calculatePrice($productName);
+function getBasePrice($product) {
+    $basePrice = $_GET[$product];
+    return [$product, $basePrice];
 }
 
-function calculatePrice() {
+function calculateFinalPrice($product) {
+    $basePrice = getBasePrice($product)[1];
     $discountPercentage = 0.1;
     $vatPercentage = 0.15;
     $discountedAmount = $basePrice * $discountPercentage;
     $discountedBasePrice = $basePrice - $discountedAmount;
     $vatAmountCharged = $basePrice * $vatPercentage;
     $finalBasePrice = $basePrice + $vatAmountCharged - $discountedAmount;
-    echo $finalBasePrice;
-  }
-
-// Valid options. A whitelist of allowed options.
-$productOptions = array(
-    'product1',
-    'product2'
-);
-
-// Empty array by default.
-$products = array();
-
-// If the POST var "products" is a valid array.
-if(!empty($_POST['products']) && is_array($_POST['products'])) {
-    //Loop through the array of checkbox values.
-    foreach($_POST['products'] as $product){
-        //Make sure that this option is a valid one.
-        if(in_array($product, $productOptions)){
-            //Add the selected options to our $products array.
-            $products[] = $product;
-        }
-    }
+    return [$discountedAmount, $discountedBasePrice, $vatAmountCharged, $finalBasePrice];
 }
 
-var_dump($produts);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // call the product selected name and base price for `product1` and `product2`.
+    $productSelected1 = getBasePrice("product1")[0];
+    $basePrice1 = getBasePrice("product1")[1];
+    $productSelected2 = getBasePrice("product2")[0];
+    $basePrice2 = getBasePrice("product2")[1];
+    // call the calculated `discounted amount`, `base price after discount`, `vat amount` and `the final price the user will be paying`
+    $discountedAmount1 = calculateFinalPrice("product1")[0];
+    $discountedBasePrice1 = calculateFinalPrice("product1")[1];
+    $vatAmountCharged1 = calculateFinalPrice("product1")[2];
+    $vatAmountCharged1 = calculateFinalPrice("product1")[3];
+    $discountedAmount2 = calculateFinalPrice("product1")[0];
+    $discountedBasePrice2 = calculateFinalPrice("product1")[1];
+    $vatAmountCharged2 = calculateFinalPrice("product1")[2];
+    $vatAmountCharged2 = calculateFinalPrice("product1")[3];
+}
 
-if (isset($_POST['products'])) {
-    calculatePrice();
+if ($productSelected1) {
+    echo "The product {$productSelected1} has base price {$basePrice1},
+    the discounted amount is {$discountedAmount1}
+    the discounted base price is then {$discountedBasePrice1},
+    the amount of VAT charged is {$vatAmountCharged1} and so
+    the final price is {$finalBasePrice1}";
+} else {
+    echo "The product {$productSelected2} has base price {$basePrice2},
+    the discounted amount is {$discountedAmount2}
+    the discounted base price is then {$discountedBasePrice2},
+    the amount of VAT charged is {$vatAmountCharged2} and so
+    the final price is {$finalBasePrice2}";
 }
 
 ?>
-<form action="" method="post">
-    <label>
-        Product1
-        <input type="checkbox" name="products[]" value="product1">
-    </label>
-    <label>
-		Product2
-        <input type="checkbox" name="products[]" value="product2">
-    </label>
-    <input type="submit" value="Submit">
-</form>
 <iframe src="task2.txt" height="400" width="1200">
     Your browser does not support iframes.
 </iframe>
--- Assignment3/task3.php
+++ Assignment3/task3.php
@@ -3,40 +3,37 @@
 <head>
 </head>
 <body>
-    <h2>Registration for hackathon</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-        Name: <input type="text" name="name">
-        <br><br>
-        E-mail: <input type="text" name="email">
-        <br><br>
-        Role:
-        <input type="checkbox" name="role" value="Project Leader">Project Leader
-        <input type="checkbox" name="role" value="Programmer">Programmer
-        <input type="checkbox" name="role" value="other">Tester
-        <br><br>
-        Dietary Requirements:
-        <select name="diet">
-            <option value="Select">Select</option>
-            <option value="None">None</option>
-            <option value="Vegetarian">Vegetarian</option>
-            <option value="Banting">Banting</option>
-        </select>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-</body>
-</html>
 
 <?php
 include 'menu.inc';
 // define variables and set to empty values
-// $name, $email, $role;
+$nameErr = $emailErr = $roleErr = $dietErr = "";
+$name = $email = $role = $diet = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = test_input($_POST["name"]);
-    $email = test_input($_POST["email"]);
-    $role = test_input($_POST["role"]);
-    $diet = test_input($_POST["diet"]);
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+      } else {
+        $email = test_input($_POST["email"]);
+      }
+
+    if (empty($_POST["role"])) {
+        $genderErr = "At lest one role is required";
+    } else {
+        $role = test_input($_POST["role"]);
+    }
+
+    if (empty($_POST["diet"])) {
+        $genderErr = "At lest one diet is required";
+    } else {
+        $diet = test_input($_POST["diet"]);
+    }
 }
 
 function test_input($data) {
@@ -56,6 +53,34 @@ echo "<br>";
 echo "with dietary requirements <b>{$diet}</b>";
 echo "<br>";
 ?>
+
+<h2>Registration for hackathon</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        Name: <input type="text" name="name">
+        <span class="error">* <?php echo $nameErr;?></span>
+        <br><br>
+        E-mail: <input type="text" name="email">
+        <span class="error">* <?php echo $emailErr;?></span>
+        <br><br>
+        Role:
+        <input type="checkbox" name="role" value="Project Leader">Project Leader
+        <input type="checkbox" name="role" value="Programmer">Programmer
+        <input type="checkbox" name="role" value="other">Tester
+        <span class="error">* <?php echo roleErr;?></span>
+        <br><br>
+        Dietary Requirements:
+        <select name="diet">
+            <option value="Select">Select</option>
+            <option value="None">None</option>
+            <option value="Vegetarian">Vegetarian</option>
+            <option value="Banting">Banting</option>
+        </select>
+        <span class="error">* <?php echo $dietErr;?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
 
 <iframe src="task3.txt" height="400" width="1200">
     Your browser does not support iframes.
